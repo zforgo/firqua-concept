c1de56dd7a7f3072c4acf9b863cfc03ac325a750
zforgo <zforgo@gmail.com>
2026-09-07 10:32:13 +0200

    Improved Store handling in Liquibase migration, thread visibility and cleaning

--- tools/test-data-tools/src/main/java/io/github/zforgo/firqua/test/liquibase/DescriptionHolder.java
+++ tools/test-data-tools/src/main/java/io/github/zforgo/firqua/test/liquibase/DescriptionHolder.java
@@ -1,24 +1,28 @@
 package io.github.zforgo.firqua.test.liquibase;
 
 import java.lang.reflect.Method;
-import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 final class DescriptionHolder {
 
     private record Description(Class<?> cls, Method method) {}
 
-    private static final ThreadLocal<Set<Description>> STORE = ThreadLocal.withInitial(HashSet::new);
+    private static final Set<Description> STORE = ConcurrentHashMap.newKeySet();
 
     public static boolean hasClass(Class<?> cls) {
-        return STORE.get().stream().anyMatch(d -> d.cls.equals(cls));
+        return STORE.stream().anyMatch(d -> d.cls.equals(cls));
     }
 
     public static boolean hasMethod(Class<?> cls, Method method) {
-        return STORE.get().contains(new Description(cls, method));
+        return STORE.contains(new Description(cls, method));
     }
 
     public static void store(Class<?> cls, Method method) {
-        STORE.get().add(new Description(cls, method));
+        STORE.add(new Description(cls, method));
+    }
+
+    public static void discard(Class<?> cls) {
+        STORE.removeIf(d -> d.cls.equals(cls));
     }
 }
